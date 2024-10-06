af64ddb0a6d268ace6ec70ed465e5a3cf5fd7984
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-06 20:08:34 +0100

    feat: customize release

---
+++ changelogithub.config.ts
@@ -0,0 +1,10 @@
+/** changelogithub.config.js */
+/** @type {import('npm:changelogithub').ChangelogOptions} */
+export default {
+  types: {
+    solve: { title: "✍️ New Problems Solved!" },
+    feat: { title: "🚀 Features" },
+    fix: { title: "🐞 Bug Fixes" },
+    chore: { title: "🏡 Chores" },
+  },
+};
