d45f576f3490c8c3643a4b1ba875352df69a21a1
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-06 20:13:19 +0100

    fix(changeloghub): solve -> solved

--- changelogithub.config.ts
+++ changelogithub.config.ts
@@ -2,7 +2,7 @@
 /** @type {import('npm:changelogithub').ChangelogOptions} */
 export default {
   types: {
-    solve: { title: "✍️ New Problems Solved!" },
+    solved: { title: "✍️ New Problems Solved!" },
     feat: { title: "🚀 Features" },
     fix: { title: "🐞 Bug Fixes" },
     chore: { title: "🏡 Chores" },
