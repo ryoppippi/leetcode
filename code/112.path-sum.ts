cbdbb315913a2616f79cb7f4255e4423b0933474
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-01-28 22:35:25 +0000

    solved: 112

---
+++ code/112.path-sum.ts
@@ -0,0 +1,37 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  if (root == null) return false; // empty root is false
+
+  const restSum = targetSum - root.val;
+
+  if (root.right == null && root.left == null && restSum === 0) return true; // if current root is the end and targetSum is zero, return true;
+
+  return hasPathSum(root.right, restSum) || hasPathSum(root.left, restSum);
+}
+// @leet end
+
