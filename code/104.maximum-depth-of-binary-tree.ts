0c13668832d6f3b2710ac63fdb9df05d2371beb8
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-01-28 22:02:38 +0000

    solved: 104

---
+++ code/104.maximum-depth-of-binary-tree.ts
@@ -0,0 +1,33 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+  if (root == null) return 0;
+  // if (root.left == null && root.right == null) return 1; // not needed because we are adding 1 in the return statement
+
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
+// @leet end
